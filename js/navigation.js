6ba3aed599d434d61c961e26a45e7963917247fb
Charly Drössler <charly.droessler@yahoo.de>
2026-07-21 08:39:46 +0200

    Update 21072026 0839

---
+++ js/navigation.js
@@ -0,0 +1,25 @@
+/*
+====================================================
+
+C.D. Music Artists
+
+navigation.js
+
+====================================================
+*/
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
